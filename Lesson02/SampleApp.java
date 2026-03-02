6aeae78d4be58a8663e452bec411a62635097e29
Luís Falcão <lfalcao@cc.isel.ipl.pt>
2026-03-02 02:50:05 +0000

    Lesson 02: Java Ecosystem and language inteoperability

---
+++ Lesson02/SampleApp.java
@@ -0,0 +1,14 @@
+class B1 {
+
+}
+
+class SampleApp {
+    public static void main(String[] args) {
+        main();
+    }
+
+    private static void main() {
+        System.out.println("Glorioso, o melhor clube do mundo.");
+        (new B()).sayHello();
+    }
+}
---
+++ Lesson02/SampleApp.kt
@@ -0,0 +1,22 @@
+public data class A(val x: Int)
+
+public class B {
+    fun sayHello() { 
+        println("Hello")
+    }
+}
+
+public enum class C {
+    FIRST, SECOND, THIRD
+}   
+
+public interface D
+
+
+public object E
+
+fun main() {
+    println("Glorioso, SLB!")
+    B().sayHello()
+
+}
